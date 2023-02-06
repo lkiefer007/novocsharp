e3a9543e5c385636f62031e14ee5b0b367f589d6
Leandro Kiefer <cmsleandrokiefer@gmail.com>
2023-02-06 20:43:03 -0300

    encontro remoto 4

--- Program.cs
+++ Program.cs
@@ -1,33 +1,31 @@
 ﻿using uc12.classes;
-
+using System.Text.RegularExpressions;
 
 
 
 // cadastro da pessoa fisica
 //Endereço pessoa fisica
 
-Endereco endPf1 = new Endereco();
-endPf1.Logradouro = "Rua Prest";
-endPf1.Numero = 16;
-endPf1.Comercial = false;
+// Endereco endPf1 = new Endereco();
+// endPf1.Logradouro = "Rua Prest";
+// endPf1.Numero = 16;
+// endPf1.Comercial = false;
 
-//pessoa fisica 1
-PessoaFisica Pf1 = new PessoaFisica();
-Pf1.Nome = "Leandro";
-Pf1.DataNascimento = new DateTime(1989,06,27);
-Pf1.Cpf = "125.959.497-13";
-Pf1.Rendimento = 3629;
-Pf1.Endereco = endPf1;
-
-
-//pessoa fisica 2
-PessoaFisica thePeople = new PessoaFisica("20915749788");
-thePeople.Nome = "Jo";
-//thePeople.Cpf = "125.959.497-13";
-thePeople.Rendimento = 3629;
-thePeople.Endereco = endPf1;
+// //pessoa fisica 1
+// PessoaFisica Pf1 = new PessoaFisica();
+// Pf1.Nome = "Leandro";
+// Pf1.DataNascimento = new DateTime(1989,06,27);
+// Pf1.Cpf = "125.959.497-13";
+// Pf1.Rendimento = 3629;
+// Pf1.Endereco = endPf1;
 
 
+// //pessoa fisica 2
+// PessoaFisica thePeople = new PessoaFisica("20915749788");
+// thePeople.Nome = "Jo";
+// //thePeople.Cpf = "125.959.497-13";
+// thePeople.Rendimento = 3629;
+// thePeople.Endereco = endPf1;
 
 
 
@@ -35,33 +33,92 @@ thePeople.Endereco = endPf1;
 
 
 
-//exibição dos dados
-Console.WriteLine($"PESSOA FISICA 1");
-
-Console.WriteLine($"Nome:{Pf1.Nome}");
-Console.WriteLine($"CPF:{Pf1.Cpf}");
-Console.WriteLine($"Rendimento:{Pf1.Rendimento}");
-Console.WriteLine($"Data Nascimento:{Pf1.DataNascimento}");
-Console.WriteLine($"Maior de Idade?:{Pf1.ValidarDataNascimento(Pf1.DataNascimento)}");
-Console.WriteLine($"Rua:{Pf1.Endereco.Logradouro}");
-Console.WriteLine($"N:{Pf1.Endereco.Numero}");
-Console.WriteLine($"Endereço Comercial?:{Pf1.Endereco.Comercial}");
-
-Console.WriteLine();
 
 
-Console.WriteLine($"PESSOA FISICA 2");
+// //exibição dos dados
+// Console.WriteLine($"PESSOA FISICA 1");
 
-Console.WriteLine($"Nome:{thePeople.Nome}");
-Console.WriteLine($"CPF:{thePeople.Cpf}");
-Console.WriteLine($"Rendimento:{thePeople.Rendimento}");
-Console.WriteLine($"Data Nascimento:{thePeople.DataNascimento}");
-Console.WriteLine($"Maior de Idade?:{thePeople.ValidarDataNascimento(thePeople.DataNascimento)}");
-Console.WriteLine($"Rua:{thePeople.Endereco.Logradouro}");
-Console.WriteLine($"N:{thePeople.Endereco.Numero}");
-Console.WriteLine($"Endereço Comercial?:{thePeople.Endereco.Comercial}");
+// Console.WriteLine($"Nome:{Pf1.Nome}");
+// Console.WriteLine($"CPF:{Pf1.Cpf}");
+// Console.WriteLine($"Rendimento:{Pf1.Rendimento}");
+// Console.WriteLine($"Data Nascimento:{Pf1.DataNascimento}");
+// Console.WriteLine($"Maior de Idade?:{Pf1.ValidarDataNascimento(Pf1.DataNascimento)}");
+// Console.WriteLine($"Rua:{Pf1.Endereco.Logradouro}");
+// Console.WriteLine($"N:{Pf1.Endereco.Numero}");
+// Console.WriteLine($"Endereço Comercial?:{Pf1.Endereco.Comercial}");
+
+// Console.WriteLine();
+
+
+// Console.WriteLine($"PESSOA FISICA 2");
+
+// Console.WriteLine($"Nome:{thePeople.Nome}");
+// Console.WriteLine($"CPF:{thePeople.Cpf}");
+// Console.WriteLine($"Rendimento:{thePeople.Rendimento}");
+// Console.WriteLine($"Data Nascimento:{thePeople.DataNascimento}");
+// Console.WriteLine($"Maior de Idade?:{thePeople.ValidarDataNascimento(thePeople.DataNascimento)}");
+// Console.WriteLine($"Rua:{thePeople.Endereco.Logradouro}");
+// Console.WriteLine($"N:{thePeople.Endereco.Numero}");
+// Console.WriteLine($"Endereço Comercial?:{thePeople.Endereco.Comercial}");
+
+
+
+
+
+//*****************cadastro de pessoa Jurídica***************************
+
+Endereco endPj = new Endereco();
+endPj.Logradouro = "Rua Niteroi";
+endPj.Numero = 180;
+endPj.Comercial = true;
+
+PessoaJuridica novaPj = new PessoaJuridica();
+
+novaPj.Nome = "Paulo";
+novaPj.Endereco = endPj;
+novaPj.Rendimento = 100000;
+//novaPj.Cnpj = "62.236.353/0001-42";
+novaPj.Cnpj = "62236353000142";
+novaPj.Fantasia = "SENAI";
+novaPj.RazaoSocial = "Serviço Nacional de Aprendizagem Industrial";
+
+// Console.WriteLine($"Qual é o Rendimento?");
+// Console.WriteLine($"Text");
+//Console.WriteLine($"Nome Fantasia?");
+//novaPj.Fantasia = Console.writeLine($"Nome Fantasia?")
+
+//Exibição dos Dados
+Console.WriteLine(@$"
+Razão Social: {novaPj.RazaoSocial}
+Nome Fantasia: {novaPj.Fantasia}
+Representate: {novaPj.Nome}
+CNPJ:{novaPj.Cnpj}
+CNPJ Válido: {novaPj.ValidadeCnpj(novaPj.Cnpj)}
+Rendimento Anual: {novaPj.Rendimento}
+Endereço: {novaPj.Endereco.Logradouro},{novaPj.Endereco.Numero}
+Endereço Comercial? {novaPj.Endereco.Comercial}
+");
+
+
 
 
 // DateTime data = new DateTime(1989,06,27);
 // Console.WriteLine( thePeople.ValidarDataNascimento(data));
+
+//Substring
+// string nome = "Leandro Kiefer";
+// Console.WriteLine(nome.Substring(3));
+// Console.WriteLine(nome.Substring(8));
+
+
+// Console.WriteLine(nome.Substring(0, 7));
+// Console.WriteLine(nome.Length);
+// Console.WriteLine (nome.Substring(4, nome.Length - 4));
+
+
+
+
+
+
+
 
--- classes/PessoaJuridica.cs
+++ classes/PessoaJuridica.cs
@@ -1,13 +1,16 @@
 using uc12.interfaces;
+using System.Text.RegularExpressions;
 
 namespace uc12.classes
 {
     public class PessoaJuridica : Pessoa, IPessoaJuridica
     {
 
-        public string ?Cnpj {get;set;}
+        public string? Cnpj { get; set; }
 
-        public string ?RazaoSocial {get;set;}
+        public string? RazaoSocial { get; set; }
+
+        public string? Fantasia { get; set; }
 
         public override float PagarImposto(float rendimento)
         {
@@ -16,7 +19,34 @@ namespace uc12.classes
 
         public bool ValidadeCnpj(string cnpj)
         {
-            throw new NotImplementedException();
+            //novaPj.Cnpj = "62.236.353/0001-42"; 18digitos
+            //novaPj.Cnpj = "62236353000142";     14digitos
+            //verificar se tem o 0001
+
+            if (Regex.IsMatch(cnpj, @"^(\d{2}.\d{3}.\d{3}/\d{4}-\d{2})|(\d{14})$"))
+            {
+
+                if (cnpj.Length == 18)
+                {//18 digitos
+
+                    if (cnpj.Substring(11, 4) == "0001")
+                    { //0001
+                        return true;
+
+                    }
+                    else if (cnpj.Length == 14)
+                    {//14 digitos
+
+                        if (cnpj.Substring(8, 4) == "0001")
+                        { //0001
+                            return true;
+                        }
+                    }
+                    
+                }
+
+            }
+            return false;
         }
     }
 }
